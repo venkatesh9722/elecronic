212b9b0a172f921eeac7c2ea3dfff966b9caf967
unknown <mindc1may274@iiht.tech>
2022-07-17 12:05:36 +0530

    main

---
+++ src/test/java/com/example/Electronics/ElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
